c13ae7640600e05dd2dfa7f6fbc50fbd288794ce
Radmil88 <radzio_zet@tlen.pl>
2023-06-09 13:14:52 +0200

    Zadanie dzień 4

--- Wyzwanie21/Wyzwanie21/Program.cs
+++ Wyzwanie21/Wyzwanie21/Program.cs
@@ -1,18 +1,21 @@
-﻿using System.Runtime.CompilerServices;
+﻿string name = "Ewa";
+bool female = false;
+var age =17;
 
-string name = "Ewa";
-bool female = true;
-var age = 33;
+if (female)
+{
+    if (age < 30)
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    else if (age == 33 && name == "Ewa")
+        Console.WriteLine("Ewa, lat 33");
+    else
+        Console.WriteLine("Inna kobieta");
+}
 
-if (female == true && age < 30)
+else
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-if (female == true && age == 33 && name == "Ewa")
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-if(!(female == true) && age < 18)
-{
-    Console.WriteLine("Niepełnoletni Mężczyzna");
+    if (!female && age < 18)
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    else
+        Console.WriteLine("Inny mężczyzna)");
 }
