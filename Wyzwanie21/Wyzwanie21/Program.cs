8e18cc866ac53aeaa19f8e2fe0324c53f7b2a905
Radmil88 <radzio_zet@tlen.pl>
2023-06-09 11:31:10 +0200

    Zadanie domowe dzień 4

--- Wyzwanie21/Wyzwanie21/Program.cs
+++ Wyzwanie21/Wyzwanie21/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+
+string name = "Ewa";
+bool female = true;
+var age = 33;
+
+if (female == true && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+if (female == true && age == 33 && name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+if(!(female == true) && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
