8f0b7ddd4622b06fceea0229f7fb9a7ecca59efe
Radmil88 <radzio_zet@tlen.pl>
2023-06-13 21:21:57 +0200

    Zadanie 5

--- Wyzwanie21/Wyzwanie21/Program.cs
+++ Wyzwanie21/Wyzwanie21/Program.cs
@@ -1,21 +1,70 @@
-﻿string name = "Ewa";
-bool female = false;
-var age =17;
+﻿int number = 67411118;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
 
-if (female)
-{
-    if (age < 30)
-        Console.WriteLine("Kobieta poniżej 30 lat");
-    else if (age == 33 && name == "Ewa")
-        Console.WriteLine("Ewa, lat 33");
-    else
-        Console.WriteLine("Inna kobieta");
-}
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
 
-else
+
+foreach (char letter in letters)
 {
-    if (!female && age < 18)
-        Console.WriteLine("Niepełnoletni mężczyzna");
-    else
-        Console.WriteLine("Inny mężczyzna)");
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
 }
+Console.WriteLine("Ilość cyfr w podanej liczbie " + number);
+Console.WriteLine("0 =>" + counter0);
+Console.WriteLine("1 =>" + counter1);
+Console.WriteLine("2 =>" + counter2);
+Console.WriteLine("3 =>" + counter3);
+Console.WriteLine("4 =>" + counter4);
+Console.WriteLine("5 =>" + counter5);
+Console.WriteLine("6 =>" + counter6);
+Console.WriteLine("7 =>" + counter7);
+Console.WriteLine("8 =>" + counter8);
+Console.WriteLine("9 =>" + counter9);
